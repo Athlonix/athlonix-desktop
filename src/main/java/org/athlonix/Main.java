a7ca21b7987350df5afd516eba08d45bac000a66
Antony David <antonydavid945@gmail.com>
2024-04-02 21:52:26 +0200

    init repo

---
+++ src/main/java/org/athlonix/Main.java
@@ -0,0 +1,7 @@
+package org.athlonix;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/org/athlonix/MainTest.java
@@ -0,0 +1,13 @@
+package org.athlonix;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    @DisplayName("Test main method")
+    void testMain() {
+        Main.main(new String[0]);
+    }
+}
