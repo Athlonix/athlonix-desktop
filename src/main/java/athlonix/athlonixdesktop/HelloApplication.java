486a64300e6770b4952ada9498435741f5928e03
Metololo <93775692+Metololo@users.noreply.github.com>
2024-04-14 16:16:08 +0200

    style: setup css themes stylesheets

--- src/main/java/athlonix/athlonixdesktop/HelloApplication.java
+++ src/main/java/athlonix/athlonixdesktop/HelloApplication.java
@@ -4,14 +4,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
+
+import static javafx.stage.StageStyle.DECORATED;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
+        scene.getStylesheets().add(HelloApplication.class.getResource("/style/primer-light.css").toExternalForm());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
